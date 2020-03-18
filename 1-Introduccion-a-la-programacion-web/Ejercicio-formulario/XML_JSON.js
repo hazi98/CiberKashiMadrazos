cb6088fee87fddbb29271e9c371c3a9538264bcf
Maro <misayagami98@gmail.com>
2020-03-18 14:16:37 -0600

    Ya integrado la parte de continuar XML JSON

---
+++ 1-Introduccion-a-la-programacion-web/Ejercicio-formulario/XML_JSON.js
@@ -0,0 +1,66 @@
+var boolDataMode_XML = 1;
+var boolDataMode_JSON = 2;
+var boolDataMode = boolDataMode_JSON;
+
+var txt = '<clientes>';
+txt += '<cliente>';
+txt += '<nombre>Jassiel</nombre>';
+txt += '<edad>21</edad>';
+txt += '<genero>Hombre</genero>';
+txt += '<correo>dfghj@fghj.com</correo>';
+txt += '<cel>12345678</cel>';
+txt += '</cliente>';
+txt += '</clientes>';
+
+var parser = new DOMParser();
+var xmlDoc = parser.parseFromString(txt, "text/xml");
+
+var txtj = '{"cliente":['
+        +'{"nombre":Jassiel, "edad": "21","genero":"Hombre", "correo":"dfghj@fghj.com", "cel":"12345678"}'
+        +']}';
+var clientes = JSON.parse(txtj);
+
+
+function continuar(){
+    nombre = document.getElementById("nombre").value;
+    edad = document.getElementById("edad").value;
+    genero = "";
+    if(document.getElementById("genH").checked == true)
+    {
+        genero="Hombre";
+    }
+    else{
+        if(document.getElementById("genM").checked ==true)
+        {
+            genero="Mujer";
+        }
+        else{
+            genero = "Otro";
+        }
+    } //else
+    correo = document.getElementById("correo").value;
+    cel = document.getElementById("cel").value;
+    if(boolDataMode == boolDataMode_JSON)
+    {
+        var obj = {
+        nombre: '',
+        edad: '',
+        genero:'',
+        correo:'',
+        cel:''
+        }
+        obj.nombre = nombre;
+        obj.edad = edad;
+        obj.genero = genero;
+        obj.correo = correo;
+        obj.cel = cel;
+
+        clientes.cliente.push(obj);
+        alert("Alta realizada");
+    }
+    else if (boolDataMode == boolDataMode_XML)
+    {
+
+    }
+
+}//continuar
